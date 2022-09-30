e9c800163fd5e9498a55da6f4099da2d0f54302f
vmrimberth <rimberth.vm@gmail.com>
2022-09-29 21:50:07 -0400

    Ajuste de snap carousel

--- src/screens/LugarTuristicoPhotos.js
+++ src/screens/LugarTuristicoPhotos.js
@@ -1,111 +1,92 @@
 import React, { useEffect, useState } from "react";
-import {  Text,  View, Image, StyleSheet } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import {  Text,  View, Image, Dimensions, StyleSheet } from 'react-native';
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
+
+const { width: screenWidth } = Dimensions.get('window')
 
 const LugarTuristicoPhotos = () => {
 
   const [photosList, setPhotosListList] = useState([]);
+  
 
   useEffect(() => {
     getListUrlPhotos();
- }, []);
+  }, []);
 
+  const getListUrlPhotos = () =>{
+    firestore()
+    .collection('lugar_turistico')
+    .onSnapshot(async (fLugarTuristicoList) => {
+      let tempPhotList = [];
+      let promiseImages = [];
+      fLugarTuristicoList.forEach(fLugarTuristico => {
+        tempPhotList.push({title: fLugarTuristico.data().nombre, image:''});
+        promiseImages.push(storage().ref(fLugarTuristico.data().imagen).getDownloadURL());
+      });
+      const resultPromises = await Promise.all(promiseImages);
+      resultPromises.forEach((url, index) => {
+        tempPhotList[index].image = url;
+      })
+      //console.log(tempPhotList)
+      setPhotosListList(tempPhotList);
+    });
+    //.then();
+  };
 
- const getListUrlPhotos = () =>{
-     firestore()
-     .collection('lugar_turistico')
-     //.get()
-     .onSnapshot(async (fLugarTuristicoList) => {
-         let tempPhotList = [];
-         let promiseImages = [];
-         fLugarTuristicoList.forEach(fLugarTuristico => {
-             tempPhotList.push({title: fLugarTuristico.data().nombre, image:''});
-             promiseImages.push(storage().ref(fLugarTuristico.data().imagen).getDownloadURL());
-         });
-         const resultPromises = await Promise.all(promiseImages);
-         resultPromises.forEach((url, index) => {
-          tempPhotList[index].image = url;
-        })
-         console.log(tempPhotList)
-         setPhotosListList(tempPhotList);
-     });
-     //.then();
- };
-
-  const carouselItems = [
-    {
-      title:"Item 1",
-      text: "Text 1",
-    },
-    {
-        title:"Item 2",
-        text: "Text 2",
-    },
-    {
-        title:"Item 3",
-        text: "Text 3",
-    },
-    {
-        title:"Item 4",
-        text: "Text 4",
-    },
-    {
-        title:"Item 5",
-        text: "Text 5",
-    },
-  ];
-    
-
-    function _renderItem({item}) {
-        return (
-          <View style={styles.carouselItemContainer}>
-            <Text style={styles.carouselItemTitle}>{item.title}</Text>
-            <Image style={styles.carouserItemImage} source={{uri:`${item.image}`}}/>
-          </View>
-
-        )
+  const _renderItem = ({item, index}, parallaxProps) => {
+    return (
+      <View style={styles.item}>
+        <ParallaxImage
+          source={{ uri: `${item.image}` }}
+          containerStyle={styles.imageContainer}
+          style={styles.image}
+          parallaxFactor={0.4}
+          {...parallaxProps}
+        />
+        <Text style={styles.title} numberOfLines={2}>
+          { item.title }
+        </Text>
+      </View>)
     }
 
     return (
-            <View style={styles.container}>
-                  <Carousel
-                  layout={'tinder'}
-                  layoutCardOffset={10}
-                  data={photosList}
-                  sliderWidth={300}
-                  itemWidth={300}
-                  renderItem={_renderItem}
-                  />
-            </View>
+      <View style={styles.container}>
+        <Carousel
+          sliderWidth={screenWidth}
+          sliderHeight={screenWidth}
+          itemWidth={screenWidth - 70}
+          data={photosList}
+          renderItem={_renderItem}
+          hasParallaxImages={true}
+          />
+      </View>
     );
 }
 
 const styles = StyleSheet.create({
-  container : {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }, 
-  carouselItemContainer : {
-    backgroundColor: '#333',
-    borderRadius: 4,
-    height: 300,
-    padding: 20,
-    marginTop: 20
-  }, 
-  carouserItemImage : {
-    width: '100%',
-    height: 200,
-    borderRadius: 4
+  item: {
+    marginTop:30,
+    width: screenWidth-60,
+    height: screenWidth + 80,
   },
-  carouselItemTitle : {
-    fontSize: 32,
+  imageContainer: {
+    flex: 1,
+    marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
+  },
+  title : {
+    fontSize:24,
+    color: '#0009',
     fontWeight: 'bold',
-    color: '#fff'
+    textAlign: 'center'
   }
-});
+})
 
 export default LugarTuristicoPhotos
